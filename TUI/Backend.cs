53768c3fcec4cc1eebe7fb49801eb37ee6c4508b
JackenQuake <jackenquake@gmail.com>
2022-02-24 18:42:55 +0300

    TUI fundamentals

---
+++ TUI/Backend.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// These classes represent backend to show TUI on particular system. They serve two purposes:
+// 1. Abstraction. While default implementation uses Console class of .Net Core, other backends are possible
+//    - ANSI codes for terminal emulation on *nix systems (or maybe to show TUI over network on remote terminal)
+//    - rendering in graphics mode to use TUI under GUI
+//    - direct output to video memory in text modes, should .Net ever be implemented for such OS
+// 2. Bufferization.
+//    Console output is pretty slow, to the point that screen updates are literally visible
+//    And it can greatly decrease performance for many overlapping windows, with many updates overwriting each other
+//    So a special BufferBackend can be added, saving all updates in memory buffer, which should be very fast
+//    And once everything is done, only areas that were changed are copied to screen
+//
+// Usage note: after any writing DoneUpdate() should be called to copy all buffers and caches to screen
+//
+// Techincal note about implementaion internal detail:
+// initially all buffers are allocated equal to backend dimensions, to save memory if backend is never resized (some backends cannot)
+// however, as soon as backend starts growing, buffers are reallocated to double amount required to have fewer reallocate requests
+
+namespace TUI {
+    // ---------- Abstract backend, root of the backend hierarchy
+    public abstract class Backend {
+        // ---------- Default backend. Current implementation assigns this to default C# console
+        public static Backend DefaultBackend;
+
+        // ---------- Buffering backend optionally attached to this one
+        protected BufferBackend buffer_backend;
+
+        public void AttachBufferBackend(BufferBackend backend) {
+            if (buffer_backend != null) throw new InvalidOperationException("TUI: Buffer backend already attached.");
+            buffer_backend = backend; backend.Resize(w, h);
+            if (DefaultBackend == this) DefaultBackend = backend;
+        }
+
+        // ---------- Dimensions management
+        protected int w, h;                // Width and height of the view area
+
+        public int Width { get => w; }
+        public int Height { get => h; }
+        public virtual void Resize(int _w, int _h) { w = _w; h = _h; if (buffer_backend != null) buffer_backend.Resize(w, h); }
+
+        // ---------- Utility function to verify coordinates
+        bool UseExceptions;                // Whether exceptions should be thrown on attempts to access out-of-bounds cells
+
+        protected bool Validate(int x, int y) {
+            if ((x < 0) || (x >= w)) if (UseExceptions) throw new ArgumentOutOfRangeException("x", "TUI: coordinates outside output area"); else return false;
+            if ((y < 0) || (y >= h)) if (UseExceptions) throw new ArgumentOutOfRangeException("y", "TUI: coordinates outside output area"); else return false;
+            return true;
+        }
+
+        // ---------- Character output
+        public abstract void OutChar(int x, int y, char c, int colors);
+        public virtual char ReadChar(int x, int y) { throw new InvalidOperationException("TUI: Read operations are not implemented for this backend"); }
+        public virtual int ReadColors(int x, int y) { throw new InvalidOperationException("TUI: Read operations are not implemented for this backend"); }
+        public virtual void DoneUpdate() { }
+
+        // ---------- Cursor control
+        public int CursorX { get => GetCursorX(); }
+        public int CursorY { get => GetCursorY(); }
+        public virtual int GetCursorX() { throw new InvalidOperationException("TUI: Cursor is not implemented for this backend"); }
+        public virtual int GetCursorY() { throw new InvalidOperationException("TUI: Cursor is not implemented for this backend"); }
+        public virtual bool IsCursorVisible() { throw new InvalidOperationException("TUI: Cursor is not implemented for this backend"); }
+        public virtual void ShowCursor(int x, int y) { throw new InvalidOperationException("TUI: Cursor is not implemented for this backend"); }
+        public virtual void HideCursor() { throw new InvalidOperationException("TUI: Cursor is not implemented for this backend"); }
+
+        // ---------- Event reading
+        public abstract int GetEvent();
+
+        // ---------- Constructor
+        protected Backend(int _w, int _h) { w = _w; h = _h; UseExceptions = false; buffer_backend = null; }
+    }
+
+    // ---------- Bufferization backend, serves two purposes:
+    // - can be attached to another backend by "AttachBufferBackend"
+    // - can be used as an offscreen buffer for a frame to speed up drawing
+    public class BufferBackend : Backend {
+        protected Backend backend;    // Underlying backend
+        protected int off_x, off_y;   // Offset of this virtual frame on the underlying backend
+
+        // ---------- Buffer, its size and allocation
+        protected struct BufferSymbol {
+            public char c, old_c;
+            public int colors, old_colors;
+        };
+        protected BufferSymbol [] buffer;
+        protected int size;
+
+        public override void Resize(int _w, int _h) {
+            base.Resize(_w, _h); if (w*h <= size) return;
+            size = ((size > 0) ? 2 : 1) * w * h;  // Resize trick explained in the note above
+            buffer = new BufferSymbol[size];
+            for (int i = 0; i<size; i++) {
+                buffer[i].c = buffer[i].old_c = ' ';
+                buffer[i].colors = buffer[i].old_colors = -1;
+            }
+        }
+
+        public void Move(int _x, int _y, int _w, int _h) { off_x = _x; off_y = _y; Resize(_w, _h); }
+
+        // ---------- Constructors
+        public BufferBackend(Backend _backend, int _x, int _y, int _w, int _h) : base(0, 0) { backend = _backend; Move(_x, _y, _w, _h); }
+        public BufferBackend(Backend _backend, int _w, int _h) : this(_backend, 0, 0, _w, _h) { }
+        public BufferBackend(Backend _backend) : this(_backend, 0, 0) { }
+
+        // ---------- Character output and reading simply work with the buffer
+        public override void OutChar(int x, int y, char c, int colors) { if (Validate(x, y)) { buffer[y*w+x].c = c; buffer[y*w+x].colors = colors; } }
+        public override char ReadChar(int x, int y) { return Validate(x, y) ? buffer[y*w+x].c : ' '; }
+        public override int ReadColors(int x, int y) { return Validate(x, y) ? buffer[y*w+x].colors : -1; }
+
+        // ---------- Methods to copy buffer to the underlying backend
+        // DoneUpdate only copies cells that were changed, ForceRedraw forces full redraw
+        public override void DoneUpdate() {
+            for (int y = 0; y < h; y++)
+                for (int x = 0; x < w; x++)
+                    if ((buffer[y*w+x].c != buffer[y*w+x].old_c) || (buffer[y*w+x].colors != buffer[y*w+x].old_colors)) {
+                        backend.OutChar(off_x+x, off_y+y, buffer[y*w+x].c, buffer[y*w+x].colors);
+                        buffer[y*w+x].old_c = buffer[y*w+x].c; buffer[y*w+x].old_colors = buffer[y*w+x].colors;
+                    }
+            backend.DoneUpdate();
+        }
+
+        public void ForceRedraw() {
+            for (int y = 0; y < h; y++)
+                for (int x = 0; x < w; x++) {
+                    backend.OutChar(off_x+x, off_y+y, buffer[y*w+x].c, buffer[y*w+x].colors);
+                    buffer[y*w+x].old_c = buffer[y*w+x].c; buffer[y*w+x].old_colors = buffer[y*w+x].colors;
+                }
+        }
+
+        // ---------- Cursor control and event processing pass requests to underlying backend
+        public override int GetCursorX() { return backend.GetCursorX(); }
+        public override int GetCursorY() { return backend.GetCursorY(); }
+        public override bool IsCursorVisible() { return backend.IsCursorVisible(); }
+        public override void ShowCursor(int x, int y) { if (Validate(x, y)) backend.ShowCursor(off_x+x, off_y+y); }
+        public override void HideCursor() { backend.HideCursor(); }
+        public override int GetEvent() { return backend.GetEvent(); }
+    }
+
+    // ---------- Bufferization backend with virtual cursor implementation
+    public class BufferBackendCursor : BufferBackend {
+        protected int cursor_x, cursor_y;
+        protected bool cursor_visible;
+
+        // Default cursor just swaps background and foreground for cursor cell
+        // Descendants can implement more interesting cursors
+        protected virtual void ToggleCursor(bool show) {
+            int c = buffer[cursor_y * w + cursor_x].colors;
+            if (c != -1) { c = (c >> 4) | ((c & 0x0f) << 4); }
+            buffer[cursor_y * w + cursor_x].colors = c;
+        }
+
+        public override void DoneUpdate() { if (cursor_visible) ToggleCursor(true); base.DoneUpdate(); if (cursor_visible) ToggleCursor(false); }
+
+        public override int GetCursorX() { return cursor_x; }
+        public override int GetCursorY() { return cursor_y; }
+        public override bool IsCursorVisible() { return cursor_visible; }
+
+        public override void ShowCursor(int x, int y) { cursor_visible = Validate(x, y); cursor_x = x; cursor_y = y; }
+        public override void HideCursor() { cursor_visible = false; }
+
+        public override void Resize(int _w, int _h) { base.Resize(_w, _h); cursor_visible &= ((cursor_x < w) && (cursor_y < h)); }
+
+        public BufferBackendCursor(Backend _backend, int _x, int _y, int _w, int _h) : base(_backend, _x, _y, _w, _h) { cursor_visible = false; }
+        public BufferBackendCursor(Backend _backend, int _w, int _h) : this(_backend, 0, 0, _w, _h) { }
+        public BufferBackendCursor(Backend _backend) : this(_backend, 0, 0) { }
+    }
+
+    // ---------- Useful abstraction for console backends, caches sequences of adjacent characters to speed up output
+    public abstract class CachingBackend : Backend {
+        protected char [] cache;           // Cache buffer
+        protected int cache_size,          // Size of cache buffer
+                      cache_len,           // Amount of characters currently cached
+                      cache_x, cache_y,    // Position of the cached string
+                      cache_colors;        // Colors of the cached string
+
+        // ---------- This function actually prints cached symbols
+        protected abstract void PrintCache();
+
+        // ---------- Output functions perform caching
+        public override void OutChar(int x, int y, char c, int colors) {
+            if (!Validate(x, y)) return;
+            if (cache_len > 0) {
+                if ((cache_x + cache_len == x) && (cache_y == y) && (cache_colors == colors)) { cache[cache_len++] = c; return; }
+                PrintCache();
+            }
+            cache[0] = c; cache_len = 1; cache_x = x; cache_y = y; cache_colors = colors;
+        }
+
+        public override void DoneUpdate() { if (cache_len > 0) PrintCache(); cache_len = 0; }
+
+        // ---------- Resizing should also (re)allocate cache buffer
+        protected void AllocateCache() {
+            cache_len = 0; if (w <= cache_size) return;
+            cache_size = ((cache_size > 0) ? 2 : 1) * w;  // Resize trick explained in the note above
+            cache = new char[cache_size];
+        }
+
+        public override void Resize(int _w, int _h) { base.Resize(_w, _h); AllocateCache(); }
+
+        protected CachingBackend(int _w, int _h) : base(_w, _h) { cache_size = 0; AllocateCache(); }
+    }
+
+    // ---------- Backend implementation over standard .Net Core Console
+    public class ConsoleBackend : CachingBackend {
+        protected int output_x, output_y;   // Current output position
+        protected int output_colors;        // Current output colors
+        protected int cursor_x, cursor_y;   // Current cursor position
+        protected int cursor_visible;       // 0 = invisible, 1 = visible, 2 = visible, but hidden for screen updates
+
+        protected override void PrintCache() {
+            if (cache_colors == -1) return;
+            if (cache_colors != output_colors) {
+                Console.ForegroundColor = (ConsoleColor)(cache_colors & 0x0f);
+                Console.BackgroundColor = (ConsoleColor)((cache_colors >> 4) & 0x0f);
+                output_colors = cache_colors;
+            }
+            if (cursor_visible == 1) { cursor_visible = 2; Console.CursorVisible = false; output_y = -1; }
+            if ((output_x != cache_x) || (output_y != cache_y)) { Console.SetCursorPosition(cache_x, cache_y); output_y = cache_y; }
+            Console.Write(cache, 0, cache_len); output_x = cache_x + cache_len;
+        }
+
+        public override void DoneUpdate() {
+            base.DoneUpdate();
+            if (cursor_visible == 2) { Console.SetCursorPosition(cursor_x, cursor_y); Console.CursorVisible = true; cursor_visible = 1; }
+        }
+
+        public override int GetCursorX() { return cursor_x; }
+        public override int GetCursorY() { return cursor_y; }
+        public override bool IsCursorVisible() { return cursor_visible > 0; }
+        public override void ShowCursor(int x, int y) {
+            cursor_x = x; cursor_y = y;
+            if (cursor_visible == 2) DoneUpdate(); else { Console.SetCursorPosition(cursor_x, cursor_y); Console.CursorVisible = true; cursor_visible = 1; }
+        }
+        public override void HideCursor() { Console.CursorVisible = false; cursor_visible = 0; }
+
+        public override int GetEvent() { return 0; }
+
+        public void UpdateSize() { base.Resize(Console.WindowWidth, Console.WindowHeight); }
+
+        public override void Resize(int _w, int _h) { Console.SetWindowSize(_w, _h); UpdateSize(); }
+
+        public ConsoleBackend(bool SetDefault) : base(Console.WindowWidth, Console.WindowHeight) { if (SetDefault) DefaultBackend = this; }
+    }
+}
---
+++ TUI/Frame.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace TUI {
+    /// <summary>
+    /// Class Rectangle represents simple geometric rectangle defined by position (x, y), width and height
+    /// These could only be changed by a virtual Move(...) method, which could be overriden to process coordinate changes
+    /// It also offers some "syntactic sugar" and handy utilities:
+    /// - methods to Move without resizing and Resize without moving
+    /// - properties to access rectangle as (X, Y, W, H), as (X1, Y1, X2, Y2) or as (Left, Top, Right, Bottom), whatever user prefers
+    /// - methods to check if the rectangle is empty and to calculate intersection of two rectangles (returns null if intersection is empty)
+    /// </summary>
+    public class Rectangle {
+        protected int x, y, w, h;  // Position, width and height
+
+        public virtual void Move(int _x, int _y, int _w, int _h) { x = _x; y = _y; w = _w; h = _h; }
+        public Rectangle(int _x, int _y, int _w, int _h) { Move(_x, _y, _w, _h); }
+        public Rectangle() : this(0, 0, 0, 0) { }
+
+        public void Move(int _x, int _y) { Move(_x, _y, w, h); }
+        public void Resize(int _w, int _h) { Move(x, y, _w, _h); }
+
+        public int X { get => x; set { Move(value, y, w, h); } }
+        public int Y { get => y; set { Move(x, value, w, h); } }
+        public int W { get => w; set { Move(x, y, value, h); } }
+        public int H { get => h; set { Move(x, y, w, value); } }
+
+        public int X1 { get => x; set { Move(value, y, w, h); } }
+        public int Y1 { get => y; set { Move(x, value, w, h); } }
+        public int X2 { get => x+w; set { Move(x, y, value-x, h); } }
+        public int Y2 { get => y+h; set { Move(x, y, w, value-y); } }
+
+        public int Left { get => x; set { Move(value, y, w, h); } }
+        public int Top { get => y; set { Move(x, value, w, h); } }
+        public int Right { get => x+w; set { Move(x, y, value-x, h); } }
+        public int Bottom { get => y+h; set { Move(x, y, w, value-y); } }
+
+        public bool IsEmpty() => ((w <= 0) || (h <= 0));
+
+        public static Rectangle Intersect(Rectangle a, Rectangle b) {
+            int x, y, w, h;
+            x = Math.Max(a.x, b.x); w = Math.Min(a.x + a.w, b.x + b.w) - x;
+            y = Math.Max(a.y, b.y); h = Math.Min(a.y + a.h, b.y + b.h) - y;
+            return ((w > 0) && (h > 0)) ? new Rectangle(x, y, w, h) : null;
+        }
+    }
+
+    /// <summary>
+    /// Class Frame is the base class of the TUI visual hierarchy, but actually it is not part of the window/widget architecture
+    /// It is just a rectangular area over specified Backend with Write methods,
+    ///   which could be handy in applications that don't need whole of TUI, but just simple text interface
+    /// Class also maintains current output position and current color
+    /// </summary>
+    public class Frame : Rectangle {
+        public static Backend default_backend;
+        private Backend backend;
+        public bool OutsideException = false;  // Flag if an exception should be thrown for output outside area
+
+        public Frame(Backend _backend, int _x, int _y, int _w, int _h) : base(_x, _y, _w, _h) { backend = _backend; }
+        public Frame(int _x, int _y, int _w, int _h) : this(default_backend, _x, _y, _w, _h) { }
+        public Frame(Backend _backend) : this(_backend, 0, 0, 0, 0) { }
+        public Frame() : this(default_backend, 0, 0, 0, 0) { }
+
+        int curr_x, curr_y, default_colors;
+
+        const int AlignRight = 1;
+        const int AlignLeft = 2;
+        const int AlignCenter = 3;
+
+        private void WriteExt(char c, int width, int flags, int colors) {
+            /*
+            int i, l = 0;
+
+            width = FixWidth(width, 1); if (width < 1) return;
+
+
+
+            if (curr_x + width > )
+
+                if ((curr_x < 0) || (width <= 0) || (curr_y < 0) && (curr_y >= h))
+                    if (OutsideException) throw new InvalidOperationException("Output outside area"); else return;
+            if (align != 0) { l = align*(width-1)/2; for (i = 0; i < l; i++) backend.OutChar(curr_x++, curr_y, ' ', colors); }
+            backend.OutChar(curr_x++, curr_y, c, colors);
+            if (align == 1) { l = width-1-l; for (i = 0; i < l; i++) backend.OutChar(curr_x++, curr_y, ' ', colors); }
+            */
+        }
+
+        public void WriteMany(char c, int num, int colors) {
+            /*
+            if ((curr_x < 0) || (width <= 0) || (curr_y < 0) && (curr_y >= h))
+                if (OutsideException) throw new InvalidOperationException("Output outside area"); else return;
+            if (align != 0) { l = align*(width-1)/2; for (i = 0; i < l; i++) backend.OutChar(curr_x++, curr_y, ' ', colors); }
+            backend.OutChar(curr_x++, curr_y, c, colors);
+            if (align == 1) { l = width-1-l; for (i = 0; i < l; i++) backend.OutChar(curr_x++, curr_y, ' ', colors); }
+            */
+        }
+
+
+
+        private void WriteExt(char[] c, int index, int count, int width, int flags, int colors) { for (int i = 0; i < c.Length; i++) Write(c[i]); }
+        private void WriteExt(string s, int width, int flags, int colors) { for (int i = 0; i < s.Length; i++) Write(s[i]); }
+        /*!!!!!!*/
+        private void WriteExt(long v, int width, int flags, int colors) { Write(v.ToString()); }
+        /*!!!!!!*/
+        private void WriteExt(double v, int width, int flags, int colors) { Write(v.ToString()); }
+        private void WriteExt(object obj, int width, int flags, int colors) { Write(obj.ToString()); }
+
+
+
+        // ---------- Variations of Write(...) functions
+        public void Write(char c) { WriteExt(c, 0, 0, default_colors); }
+        public void Write(char c, int colors) { WriteExt(c, 0, 0, colors); }
+        public void Write(int x, int y, char c) { curr_x = x; curr_y = y; WriteExt(c, 0, 0, default_colors); }
+        public void Write(int x, int y, char c, int colors) { curr_x = x; curr_y = y; WriteExt(c, 0, 0, colors); }
+        public void Write(char[] c) { WriteExt(c, 0, c.Length, 0, 0, default_colors); }
+        public void Write(char[] c, int colors) { WriteExt(c, 0, c.Length, 0, 0, colors); }
+        public void Write(int x, int y, char[] c) { curr_x = x; curr_y = y; WriteExt(c, 0, c.Length, 0, 0, default_colors); }
+        public void Write(int x, int y, char[] c, int colors) { curr_x = x; curr_y = y; WriteExt(c, 0, c.Length, 0, 0, colors); }
+        public void Write(char[] c, int index, int count) { WriteExt(c, index, Math.Min(count, c.Length - index), 0, 0, default_colors); }
+        public void Write(char[] c, int index, int count, int colors) { WriteExt(c, index, Math.Min(count, c.Length - index), 0, 0, colors); }
+        public void Write(int x, int y, char[] c, int index, int count) { curr_x = x; curr_y = y; WriteExt(c, index, Math.Min(count, c.Length - index), 0, 0, default_colors); }
+        public void Write(int x, int y, char[] c, int index, int count, int colors) { curr_x = x; curr_y = y; WriteExt(c, index, Math.Min(count, c.Length - index), 0, 0, colors); }
+        public void Write(string s) { WriteExt(s, 0, 0, default_colors); }
+        public void Write(string s, int colors) { WriteExt(s, 0, 0, colors); }
+        public void Write(int x, int y, string s) { curr_x = x; curr_y = y; WriteExt(s, 0, 0, default_colors); }
+        public void Write(int x, int y, string s, int colors) { curr_x = x; curr_y = y; WriteExt(s, 0, 0, colors); }
+        public void Write(long v) { WriteExt(v, 0, 0, default_colors); }
+        public void Write(long v, int colors) { WriteExt(v, 0, 0, colors); }
+        public void Write(int x, int y, long v) { curr_x = x; curr_y = y; WriteExt(v, 0, 0, default_colors); }
+        public void Write(int x, int y, long v, int colors) { curr_x = x; curr_y = y; WriteExt(v, 0, 0, colors); }
+        public void Write(double v) { WriteExt(v, 0, 0, default_colors); }
+        public void Write(double v, int colors) { WriteExt(v, 0, 0, colors); }
+        public void Write(int x, int y, double v) { curr_x = x; curr_y = y; WriteExt(v, 0, 0, default_colors); }
+        public void Write(int x, int y, double v, int colors) { curr_x = x; curr_y = y; WriteExt(v, 0, 0, colors); }
+        public void Write(object obj) { WriteExt(obj, 0, 0, default_colors); }
+        public void Write(object obj, int colors) { WriteExt(obj, 0, 0, colors); }
+        public void Write(int x, int y, object obj) { curr_x = x; curr_y = y; WriteExt(obj, 0, 0, default_colors); }
+        public void Write(int x, int y, object obj, int colors) { curr_x = x; curr_y = y; WriteExt(obj, 0, 0, colors); }
+
+        public void WriteExt(char c, int width, int flags) { WriteExt(c, width, flags, default_colors); }
+        public void WriteExt(int x, int y, char c, int width, int flags) { curr_x = x; curr_y = y; WriteExt(c, width, flags, default_colors); }
+        public void WriteExt(int x, int y, char c, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(c, width, flags, colors); }
+        public void WriteExt(char[] c, int width, int flags) { WriteExt(c, 0, c.Length, width, flags, default_colors); }
+        public void WriteExt(char[] c, int width, int flags, int colors) { WriteExt(c, 0, c.Length, width, flags, colors); }
+        public void WriteExt(int x, int y, char[] c, int width, int flags) { curr_x = x; curr_y = y; WriteExt(c, 0, c.Length, width, flags, default_colors); }
+        public void WriteExt(int x, int y, char[] c, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(c, 0, c.Length, width, flags, colors); }
+        public void WriteExt(char[] c, int index, int count, int width, int flags) { WriteExt(c, index, Math.Min(count, c.Length - index), width, flags, default_colors); }
+        public void WriteExt(int x, int y, char[] c, int index, int count, int width, int flags) { curr_x = x; curr_y = y; WriteExt(c, index, Math.Min(count, c.Length - index), width, flags, default_colors); }
+        public void WriteExt(int x, int y, char[] c, int index, int count, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(c, index, Math.Min(count, c.Length - index), width, flags, colors); }
+        public void WriteExt(string s, int width, int flags) { WriteExt(s, width, flags, default_colors); }
+        public void WriteExt(int x, int y, string s, int width, int flags) { curr_x = x; curr_y = y; WriteExt(s, width, flags, default_colors); }
+        public void WriteExt(int x, int y, string s, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(s, width, flags, colors); }
+        public void WriteExt(long v, int width, int flags) { WriteExt(v, width, flags, default_colors); }
+        public void WriteExt(int x, int y, long v, int width, int flags) { curr_x = x; curr_y = y; WriteExt(v, width, flags, default_colors); }
+        public void WriteExt(int x, int y, long v, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(v, width, flags, colors); }
+        public void WriteExt(double v, int width, int flags) { WriteExt(v, width, flags, default_colors); }
+        public void WriteExt(int x, int y, double v, int width, int flags) { curr_x = x; curr_y = y; WriteExt(v, width, flags, default_colors); }
+        public void WriteExt(int x, int y, double v, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(v, width, flags, colors); }
+        public void WriteExt(object obj, int width, int flags) { WriteExt(obj, width, flags, default_colors); }
+        public void WriteExt(int x, int y, object obj, int width, int flags) { curr_x = x; curr_y = y; WriteExt(obj, width, flags, default_colors); }
+        public void WriteExt(int x, int y, object obj, int width, int flags, int colors) { curr_x = x; curr_y = y; WriteExt(obj, width, flags, colors); }
+
+        public void WriteMany(char c, int num) { WriteMany(c, num, default_colors); }
+        public void WriteMany(int x, int y, char c, int num) { curr_x = x; curr_y = y; WriteMany(c, num, default_colors); }
+        public void WriteMany(int x, int y, char c, int num, int colors) { curr_x = x; curr_y = y; WriteMany(c, num, colors); }
+    }
+}
